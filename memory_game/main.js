e54bc1f8f41c2e1ac3678c991b4b15ec5f6d8a4e
regbake <regbake@gmail.com>
2017-08-20 15:43:28 -0700

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,19 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Found a match");
+	}
+	else {
+		alert("Sorry, try again!");
+	}
+
+}
+	console.log("User flipped " + cardOne);
+	console.log("User flipped " + cardTwo);
