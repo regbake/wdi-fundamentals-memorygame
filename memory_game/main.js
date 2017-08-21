64657bb0c629e4d4458a994dc29f06a60654b681
regbake <regbake@gmail.com>
2017-08-20 18:02:41 -0700

    chapter10

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,19 +1,45 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  	rank: 'queen',
+  	suit: 'hearts',
+  	cardImage: 'images/queen-of-hearts.png',
+  },
+  {
+  	rank: 'queen',
+  	suit: 'diamonds',
+  	cardImage: 'images/queen-of-diamonds.png',
+  },
+  {
+  	rank: 'king',
+  	suit: 'hearts',
+  	cardImage: 'images/king-of-hearts.png',
+  },
+  {
+  	rank: 'king',
+  	suit: 'diamonds',
+  	cardImage: 'images/king-of-diamonds.png',
+  },
+];
+
 var cardsInPlay = [];
 
-var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
-
-  if (cardsInPlay.length === 2) {
-  	  if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("Found a match");
 	  }
 	  else {
 		alert("Sorry, try again!");
 	  }
-  }
+}
+var flipCard = function(cardId) {
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
 
+  if (cardsInPlay.length === 2) {
+  	  checkForMatch();
+  }
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 
 }
   flipCard(0);
